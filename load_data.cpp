8b302293f5e22bce3034fde969984dd3ed053cf4
Nguyễn Thành Long <long.itptit@gmail.com>
2021-06-07 19:09:00 +0700

    update

--- load_data.cpp
+++ load_data.cpp
@@ -12,25 +12,34 @@
 
 Matrix load_data(const char* data_path, int rows, int cols){
     freopen (data_path,"r",stdin);
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
     std::vector<double> data(0);
     for (int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
             int pixel;
             std::cin >> pixel;
             data.push_back(pixel);
+            // print(pixel);
         }
     }
     Matrix mat = Matrix(data);
+    // print(mat);
     mat.m_Rows = rows;
     mat.m_Columns = cols;
     return mat;
 }
 
 int main(){
-    Matrix mat_train_data = load_data("data/train_data.txt", 1934, 1024);
-    Matrix mat_train_label = load_data("data/train_label.txt", 1934, 1);
-    Matrix mat_valid_data = load_data("data/valid_data.txt", 500, 1024);
-    Matrix mat_valid_label = load_data("data/valid_label.txt", 500, 1);
-    Matrix mat_test_data = load_data("data/test_data.txt", 446, 1024);
-    Matrix mat_test_label = load_data("data/test_label.txt", 446, 1);
+
+    Matrix mat_valid_data = load_data("/home/long/nn-cpp/data/valid_data.txt", 500, 1024);
+    Matrix mat_valid_label = load_data("/home/long/nn-cpp/data/valid_label.txt", 500, 10);
+
+    Matrix mat_train_data = load_data("/home/long/nn-cpp/data/train_data.txt", 1934, 1024);
+    Matrix mat_train_label = load_data("/home/long/nn-cpp/data/train_label.txt", 1934, 10);
+
+    Matrix mat_test_data = load_data("/home/long/nn-cpp/data/test_data.txt", 446, 1024);
+    Matrix mat_test_label = load_data("/home/long/nn-cpp/data/test_label.txt", 446, 10);
+
+    print(mat_valid_label);
 }
