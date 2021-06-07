5e9d992b21542a6d6e7c3b748e6769ee7ef97525
Nguyễn Thành Long <long.itptit@gmail.com>
2021-06-07 18:18:55 +0700

    update

--- load_data.cpp
+++ load_data.cpp
@@ -10,24 +10,27 @@
 #define print(x) std::cout << x << std::endl
 
 
-Matrix load_data(const char* data_path, int size){
+Matrix load_data(const char* data_path, int rows, int cols){
     freopen (data_path,"r",stdin);
     std::vector<double> data(0);
-    for (int i = 0; i < size; i++){
-        int pixel;
-        std::cin >> pixel;
-        data.push_back(pixel);
+    for (int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            int pixel;
+            std::cin >> pixel;
+            data.push_back(pixel);
+        }
     }
     Matrix mat = Matrix(data);
-
+    mat.m_Rows = rows;
+    mat.m_Columns = cols;
     return mat;
 }
 
 int main(){
-    Matrix mat_train_data = load_data("data/train_data.txt", 1934 * 32 * 32);
-    Matrix mat_train_label = load_data("data/train_label.txt", 1934);
-    Matrix mat_valid_data = load_data("data/valid_data.txt", 500 * 32 * 32);
-    Matrix mat_valid_label = load_data("data/valid_label.txt", 500);
-    Matrix mat_test_data = load_data("data/test_data.txt", 446 * 32 * 32);
-    Matrix mat_test_label = load_data("data/test_label.txt", 446);
+    Matrix mat_train_data = load_data("data/train_data.txt", 1934, 1024);
+    Matrix mat_train_label = load_data("data/train_label.txt", 1934, 1);
+    Matrix mat_valid_data = load_data("data/valid_data.txt", 500, 1024);
+    Matrix mat_valid_label = load_data("data/valid_label.txt", 500, 1);
+    Matrix mat_test_data = load_data("data/test_data.txt", 446, 1024);
+    Matrix mat_test_label = load_data("data/test_label.txt", 446, 1);
 }
